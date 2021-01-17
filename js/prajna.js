114d66d3d5a560401ad0301238dffe84b82f961f
abhijitmen <33654176+abhijitmen@users.noreply.github.com>
2021-01-17 21:31:58 +0530

    updated 17.1.2021 9.30 PM

--- js/prajna.js
+++ js/prajna.js
@@ -25,3 +25,24 @@ google.maps.event.addDomListener(window, 'load', regular_map);
 $('.carousel').carousel({
     interval: 3000,
 })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
